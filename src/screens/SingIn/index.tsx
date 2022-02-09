6fddb701f37189032bc381123026212c78c7d6e9
Divino J. Silva <divino_j_silva@hotmail.com>
2022-02-09 18:12:03 -0300

    tela de login primeiros stilos e componentes

--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -1,24 +1,47 @@
-import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import React from "react";
+import { 
+  View, 
+  Text, 
+  Image,
+  StatusBar,
+ } from "react-native";
+
 import { styles } from './styles';
 
+import IllustrationImg from '../../assets/illustration.png';
+
+
 export function SingIn(){
-const [text, setText] = useState('');
-
-
 
   return(
 
     <View style={styles.container}>
-      <Text>Hello World, Olá Divino Seja bem vindo</Text>
+      
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent/>
 
-    <TextInput 
-    style={styles.input}
-    onChangeText={setText}/>
+     <Image 
+     source={IllustrationImg}
+     style={styles.image} 
+     resizeMode="stretch"
+     />
+    <View style={styles.content}>
+    <Text style={styles.title}>
+      Conecte-se {`\n`}
+      e organize suas {`\n`}
+      jogatinas
+    </Text>
+    <Text style={styles.subtitle}>
+      Crie grupos para jogar seus games {`\n`}
+      favoritos com seus amigos
+    </Text>
 
-    <Text>
-        Você Digitou: {text}
-        </Text>
+
+
+
+    </View>
 
 
     </View>
--- src/screens/SingIn/styles.ts
+++ src/screens/SingIn/styles.ts
@@ -1,17 +1,39 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/themes';
+
 
 export const styles = StyleSheet.create({
    
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
-       // backgroundColor: 'green'
+        alignItems: 'center',
+         backgroundColor: theme.colors.background
     },
 
-    input: {
-        height: 50,
-        width: 200,
-        borderBottomWidth: 2
+    image: {
+        width: '100%',
+        height: 360
+    },
+
+    content: {
+        marginTop: -40,
+    },
+
+    title: {
+        color: theme.colors.heading,
+        textAlign: 'center',
+        //fontFamily: "Rajdhani",
+        fontSize: 40,
+        marginBottom: 16
+    },
+
+    subtitle: {
+        color: theme.colors.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64
     }
+
+
 });
